babc0e6d318e0a40ccf26733e0e5b2e694553fe0
Renan <renan.fachin@hotmail.com>
2024-05-18 23:45:07 -0300

    feat: Implementa telas de autenticação com clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+import { Logo } from "@/components/Logo"
+import { ReactNode } from "react"
+
+interface layoutProps{
+  children: ReactNode
+}
+
+export default function layout({children}: layoutProps){
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <Logo />
+      <div className="mt-10">
+        {children}
+      </div>
+    </div>
+  )
+}
---
+++ components/Logo.tsx
@@ -0,0 +1,12 @@
+import { PiggyBank } from "lucide-react";
+
+export function Logo(){
+  return(
+    <a href="/" className="flex items-center gap-2">
+      <PiggyBank className="stroke size-10 stroke-emerald-500 stroke-2"/>
+      <p className="bg-gradient-to-r from-emerald-400 to-green-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        BudgetTracker
+      </p>
+    </a>
+  )
+}
